178d216b9b2bcb165102c387dcb217f0cf5b4f8f
Helder Fernandes <helder.fernandes.t0127540@edu.atec.pt>
2025-02-24 04:19:07 +0000

    added-new-events

--- script.js
+++ script.js
@@ -3,24 +3,35 @@ var taskList = [];
 document.getElementById("formNewTask").addEventListener("submit", (event) => {
     event.preventDefault(); // Prevent the form from submitting
 
-    taskList.push(task);
+    var value = document.getElementById("inputNewTask").value;
 
-    var task = document.getElementById("inputNewTask").value;
+    var newTaskItem = {
+        completed: false,
+        text: value
+    }; // Add the new task to the task list
 
-    localStorage.setItem("tasks", JSON.stringify(taskList)); // Save the data to local storage
+    taskList.push(newTaskItem); // Add the new task to the task list
 
-    newTask(task);
+    localStorage.setItem("taskList", JSON.stringify(taskList)); // Save the data to local storage
+
+    newTask(newTaskItem); // Create a new task
 });
 
 function getData() { // Function to get the data from local storage
-    var data = localStorage.getItem("tasks");
-    if (data) {
+    var data = JSON.parse(localStorage.getItem("taskList"));
+
+    if (data && data.length > 0) {
         console.log("Data retrieved", data);
+        taskList = data;
+        data.forEach(element => { // Loop through the data and create a new task for each element
+            newTask(element);
+        });
     } else {
-        console.log("No data found");
+        console.log("No data found", data);
     }
 }
 
+getData();
 
 function newTask(task) {
 
@@ -34,36 +45,33 @@ function newTask(task) {
 
     // Set the type of the input elements 
     newTaskCheckbox.type = "checkbox";
+    newTaskCheckbox.checked = task.completed; // Set the value of the new task checkbox element
+
     newTaskText.type = "text";
+    newTaskText.innerText = task.text; // Set the value of the new task text element
 
-    newTaskText.innerText = task; // Set the value of the new task text element
     newTaskEdit.innerHTML = '<img class="icon" src="/imgs/icons8-edit-100.png"></img>'; // Set the value of the new task edit button to a pencil emoji
-    newTaskDelete.innerHTML = '<img class="icon" src="/imgs/icons8-edit-100.png"></img>'; // Set the value of the new task delete button to a garbage emoji
+    newTaskEdit.disabled = task.completed; // Disable the edit button if the task is completed
 
-    // Append the new task elements to the DOM
-    newTaskLabel.appendChild(newTaskCheckbox);
-    newTaskLabel.appendChild(newTaskText);
-    newTask.appendChild(newTaskLabel);
-    newTask.appendChild(newTaskEdit);
-    newTask.appendChild(newTaskDelete);
+    newTaskDelete.innerHTML = '<img class="icon" src="/imgs/icons8-delete-100.png"></img>'; // Set the value of the new task delete button to a garbage emoji
 
-    // Append the new task to the task list
-    var taskList = document.getElementById("taskList");
-    //taskList.appendChild(newTask); // Append the new task to the end of the list
-
-    // Insert the new task at the beginning of the list
-    taskList.insertBefore(newTask, taskList.firstChild);
-
-
-    // Add event listeners to the new task elements
-    newTaskDelete.addEventListener("click", () => {
-        newTask.remove();
+    newTaskCheckbox.addEventListener("change", () => {
+        task.completed = !task.completed; // Toggle the completed status of the task
+        localStorage.setItem("taskList", JSON.stringify(taskList)); // Save the data to local storage
+        newTaskEdit.disabled = task.completed; // Disable the edit button if the task is completed
+        console.log("Task completed", newTaskCheckbox.checked);
     });
 
     newTaskEdit.addEventListener("click", () => {
         newTaskCheckbox.disabled = true;
         newTaskEdit.disabled = true;
         newTaskText.contentEditable = true;
+        newTaskText.focus();
+    });
+
+    // Add event listeners to the new task elements
+    newTaskDelete.addEventListener("click", () => {
+        newTask.remove();
     });
 
     newTaskText.addEventListener("focusout", () => {
@@ -80,4 +88,18 @@ function newTask(task) {
         }
     });
 
+    // Append the new task elements to the DOM
+    newTaskLabel.appendChild(newTaskCheckbox);
+    newTaskLabel.appendChild(newTaskText);
+    newTask.appendChild(newTaskLabel);
+    newTask.appendChild(newTaskEdit);
+    newTask.appendChild(newTaskDelete);
+
+    // Append the new task to the list
+    var List = document.getElementById("List");
+    //taskList.appendChild(newTask); 
+    // Append the new task to the list
+
+    // Insert the new task at the beginning of the list
+    List.insertBefore(newTask, List.firstChild);
 }
